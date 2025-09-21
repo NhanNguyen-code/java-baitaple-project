2402d69fbe04083afdd56cef79609397ba607ac3
NhanNguyen-code <nguyennhanpro47@gmail.com>
2025-09-21 18:49:34 +0700

    Update KichCo.java

--- edu/fit/iuh/cs/KichCo.java
+++ edu/fit/iuh/cs/KichCo.java
@@ -1,1 +1,19 @@
 
+package edu.fit.iuh.cs;
+
+public enum KichCo {
+	NHO("Nho"), 
+	VUA("Vua"), 
+	LON("Lon");
+	
+	private String size;
+	
+	private KichCo(String size) {
+		this.size =size;
+	}
+	
+	@Override
+	public String toString() {
+		return size;
+	}
+}
