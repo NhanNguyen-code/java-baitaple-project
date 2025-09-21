2350d75c2cc8e19928b5b146dc568cea43dacad5
NhanNguyen-code <nguyennhanpro47@gmail.com>
2025-09-21 18:50:20 +0700

    Update ChatLieu.java

--- edu/fit/iuh/cs/ChatLieu.java
+++ edu/fit/iuh/cs/ChatLieu.java
@@ -1,1 +1,19 @@
 
+package edu.fit.iuh.cs;
+
+public enum ChatLieu {
+	GO("Go"),
+	NHUA("Nhua"),
+	KIMLOAI("Kim loai");
+	
+	private String material;
+	
+	private ChatLieu(String material) {
+		this.material =material;
+	}
+	
+	@Override
+	public String toString() {
+		return material;
+	}
+}
